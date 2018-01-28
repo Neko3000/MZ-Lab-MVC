82c28c16c0ce566b1e2e224311a7ae4cee6593aa
Xueliang Chen <Neko3000@live.com>
2018-01-28 23:26:43 +0900

    feat(Program.cs): Use Kestrel on HostBuilder

--- MZ-Lab-MVC/Program.cs
+++ MZ-Lab-MVC/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -14,12 +15,25 @@ namespace MZ_Lab_MVC
     {
         public static void Main(string[] args)
         {
+            //var host = new WebHostBuilder()
+            //.UseKestrel()
+            //.UseContentRoot(Directory.GetCurrentDirectory())
+            //.UseIISIntegration()
+            //.UseStartup<Startup>()
+            //.Build();
+
+            //host.Run();
+
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Loopback, 5000);
+                })
                 .Build();
     }
 }
